6a089847952a7c2089028e3af47640896b3c22ca
Matan Kushner <matchai@me.com>
2018-02-02 16:35:04 -0500

    Truncate path from git repo root

--- functions/__sf_section_dir.fish
+++ functions/__sf_section_dir.fish
@@ -1,9 +1,13 @@
 function __sf_section_dir -a dir_color -a separator_color -a branch_color -a status_color
 	if [ (__sf_util_git_branch) ]
-		echo -s -n (set_color -o $dir_color) (basename "$PWD")
+		set -l git_root (git rev-parse --show-toplevel)
+
+		# Treat the repo root as a top-level directory
+		echo -s -n (set_color -o $dir_color) (string replace $git_root (basename $git_root) $PWD)
 		echo -s -n (set_color -o $separator_color) " on "
 		echo -s -n (set_color -o $branch_color) " " (__sf_util_git_branch) (__sf_section_git $status_color)
 	else
-		echo -s -n (set_color -o $dir_color) (string replace $HOME '~' $PWD)
+		set -g fish_prompt_pwd_dir_length 0
+		echo -s -n (set_color -o $dir_color) (prompt_pwd)
 	end
 end
