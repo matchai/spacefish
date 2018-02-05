96a0f60b063bd034c4ea4387aee6c829d85cf0d4
Matan Kushner <matchai@me.com>
2018-02-05 17:35:14 -0500

    Refactor exec_time

--- functions/__sf_section_exec_time.fish
+++ functions/__sf_section_exec_time.fish
@@ -1,8 +1,34 @@
-function __sf_section_exec_time -a prefix_color -a duration_color
-	set -l command_duration
+function __sf_section_exec_time
+	#
+	# Execution time
+	#
+	# Execution time of the last command.
 
-	if test -n $CMD_DURATION -a $CMD_DURATION -gt 5000
-		set command_duration (echo $CMD_DURATION | __sf_util_human_time)
-		echo -s -n (set_color $prefix_color) "took " (set_color $duration_color) $command_duration
+	# ------------------------------------------------------------------------------
+	# Configuration
+	# ------------------------------------------------------------------------------
+
+	__sf_util_set_default SPACEFISH_EXEC_TIME_SHOW true
+	__sf_util_set_default SPACEFISH_EXEC_TIME_PREFIX "took "
+	__sf_util_set_default SPACEFISH_EXEC_TIME_SUFFIX $SPACEFISH_PROMPT_DEFAULT_SUFFIX
+	__sf_util_set_default SPACEFISH_EXEC_TIME_COLOR (set_color yellow)
+	__sf_util_set_default SPACEFISH_EXEC_TIME_ELAPSED 2
+
+	# ------------------------------------------------------------------------------
+	# Section
+	# ------------------------------------------------------------------------------
+
+	if test $SPACEFISH_EXEC_TIME_SHOW = false
+		return
+	end
+
+	if test -n $CMD_DURATION -a $CMD_DURATION -gt (math "$SPACEFISH_EXEC_TIME_ELAPSED * 1000")
+		set -l command_duration (echo $CMD_DURATION | __sf_util_human_time)
+		echo -e -n -s \
+		(set_color ffffff) \
+		$SPACEFISH_EXEC_TIME_PREFIX \
+		$SPACEFISH_EXEC_TIME_COLOR \
+		$command_duration \
+		$SPACESHIP_EXEC_TIME_SUFFIX
 	end
 end
