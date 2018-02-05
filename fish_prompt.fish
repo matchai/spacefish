b72d0b73eabbfcbc008c807ae29ae4acc1d230c1
Matan Kushner <matchai@me.com>
2018-02-05 17:08:53 -0500

    Allow for reordering of sections

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,17 +1,24 @@
 function fish_prompt
-	__sf_util_set_default SPACEFISH_PROMPT_ADD_NEWLINE true
-	__sf_util_set_default SPACEFISH_PROMPT_SEPARATE_LINE true
-	__sf_util_set_default SPACEFISH_PROMPT_FIRST_PREFIX_SHOW false
-	__sf_util_set_default SPACEFISH_PROMPT_PREFIXES_SHOW true
-	__sf_util_set_default SPACEFISH_PROMPT_SUFFIXES_SHOW true
-	__sf_util_set_default SPACEFISH_PROMPT_DEFAULT_PREFIX "via "
-	__sf_util_set_default SPACEFISH_PROMPT_DEFAULT_SUFFIX " "
+    __sf_util_set_default SPACEFISH_PROMPT_ADD_NEWLINE true
+    # __sf_util_set_default SPACEFISH_PROMPT_SEPARATE_LINE true
+    # __sf_util_set_default SPACEFISH_PROMPT_FIRST_PREFIX_SHOW false
+    # __sf_util_set_default SPACEFISH_PROMPT_PREFIXES_SHOW true
+    # __sf_util_set_default SPACEFISH_PROMPT_SUFFIXES_SHOW true
+    # __sf_util_set_default SPACEFISH_PROMPT_DEFAULT_PREFIX "via "
+    __sf_util_set_default SPACEFISH_PROMPT_DEFAULT_SUFFIX " "
 
-	echo -e ''
-	__sf_section_dir
-	__sf_section_git
-	__sf_section_exec_time $brwhite yellow
-	echo
-	__sf_section_char
-	set_color normal
+    __sf_util_set_default SPACEFISH_PROMPT_ORDER dir git exec_time echo char
+
+
+    if test $SPACEFISH_PROMPT_ADD_NEWLINE
+        echo
+    end
+
+    for i in $SPACEFISH_PROMPT_ORDER
+        if test $i = echo
+            echo
+        else
+            eval __sf_section_$i
+        end
+    end
 end
