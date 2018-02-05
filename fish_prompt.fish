4252c66f01816826785182b5b4fe08c0520e1181
Matan Kushner <matchai@me.com>
2018-02-05 12:30:34 -0500

    Refactor dir structure

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,8 +1,16 @@
 function fish_prompt
+	__sf_util_set_default SPACEFISH_PROMPT_ADD_NEWLINE true
+	__sf_util_set_default SPACEFISH_PROMPT_SEPARATE_LINE true
+	__sf_util_set_default SPACEFISH_PROMPT_FIRST_PREFIX_SHOW false
+	__sf_util_set_default SPACEFISH_PROMPT_PREFIXES_SHOW true
+	__sf_util_set_default SPACEFISH_PROMPT_SUFFIXES_SHOW true
+	__sf_util_set_default SPACEFISH_PROMPT_DEFAULT_PREFIX "via "
+	__sf_util_set_default SPACEFISH_PROMPT_DEFAULT_SUFFIX " "
+
 	set -l brwhite ffffff
 
 	echo -e ''
-	__sf_section_dir cyan $brwhite magenta red
+	__sf_section_dir $brwhite magenta red
 	__sf_section_exec_time $brwhite yellow
 	echo
 	__sf_section_char
