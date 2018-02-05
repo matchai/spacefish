79c96a980aa5652f5d65773b4d2c60cfb223a5b5
Matan Kushner <matchai@me.com>
2018-02-05 14:32:17 -0500

    Remove extra space in exec_time

--- functions/__sf_section_exec_time.fish
+++ functions/__sf_section_exec_time.fish
@@ -3,6 +3,6 @@ function __sf_section_exec_time -a prefix_color -a duration_color
 
 	if test -n $CMD_DURATION -a $CMD_DURATION -gt 5000
 		set command_duration (echo $CMD_DURATION | __sf_util_human_time)
-		echo -s -n (set_color $prefix_color) " took " (set_color $duration_color) $command_duration
+		echo -s -n (set_color $prefix_color) "took " (set_color $duration_color) $command_duration
 	end
 end
