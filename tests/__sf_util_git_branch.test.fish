c7b82a93adc9d6afe29290d9157a25c0b4d8aa2e
Matan Kushner <matchai@me.com>
2018-02-19 13:53:35 -0500

    Add tests for git_branch

---
+++ tests/__sf_util_git_branch.test.fish
@@ -0,0 +1,41 @@
+source $DIRNAME/../functions/__sf_util_git_branch.fish
+set path /var/tmp/$DIRNAME/$TESTNAME
+
+function setup
+	mkdir -p $path/tempGit
+	cd $path/tempGit
+end
+
+function teardown
+	rm -rf $path
+end
+
+test "identify HEAD before initial commit"
+	'HEAD' = (
+		command git init
+		__sf_util_git_branch
+	)
+end
+
+test "identify master branch"
+	'master' = (
+		command git init
+		command git commit --allow-empty -m "initial commit"
+		__sf_util_git_branch
+	)
+end
+
+test "indentify an alternate branch name"
+	'testBranch' = (
+		command git init
+		command git checkout -b "testBranch"
+		command git commit --allow-empty -m "initial commit"
+		__sf_util_git_branch
+	)
+end
+
+test "no result for non-git directory"
+    '' = (
+		__sf_util_git_branch
+	)
+end
