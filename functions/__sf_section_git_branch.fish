ce3b6640d6e8f926ac8c167597ce353aea9470d8
Matan Kushner <matchai@me.com>
2018-03-02 15:04:00 -0500

    Fix git branch persisting

--- functions/__sf_section_git_branch.fish
+++ functions/__sf_section_git_branch.fish
@@ -18,7 +18,11 @@ function __sf_section_git_branch -d "Format the displayed branch name"
 
 	[ $SPACEFISH_GIT_BRANCH_SHOW = false ]; and return
 
+	set -l git_branch (__sf_util_git_branch)
+
+	[ -z $git_branch ]; and return
+
 	__sf_lib_section \
 	$SPACEFISH_GIT_BRANCH_COLOR \
-	$SPACEFISH_GIT_BRANCH_PREFIX(__sf_util_git_branch)$SPACEFISH_GIT_BRANCH_SUFFIX
+	$SPACEFISH_GIT_BRANCH_PREFIX$git_branch$SPACEFISH_GIT_BRANCH_SUFFIX
 end
