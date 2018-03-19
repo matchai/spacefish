685ee9755161f7ebcedb64d9ea600af201930288
Matan Kushner <hello@matchai.me>
2018-03-19 11:31:17 -0400

    Remove setup clearing env variables

--- tests/__sf_section_char.test.fish
+++ tests/__sf_section_char.test.fish
@@ -1,13 +1,5 @@
 source $DIRNAME/../functions/__sf_section_char.fish
 
-set -e SPACEFISH_CHAR_PREFIX
-set -e SPACEFISH_CHAR_SUFFIX
-set -e SPACEFISH_CHAR_SYMBOL
-
-# Setup fails when attempting to clear these vars
-# set -e SPACEFISH_CHAR_COLOR_SUCCESS
-# set -e SPACEFISH_CHAR_COLOR_FAILURE
-
 test "Display default char with status code 0"
 	(
 		set -g sf_exit_code 0
