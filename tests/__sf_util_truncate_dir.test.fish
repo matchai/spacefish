7b0d9aaa9818430c663694d82db73077027df28d
Matan Kushner <hello@matchai.me>
2018-03-22 14:52:55 -0400

    Reword test cases

--- tests/__sf_util_truncate_dir.test.fish
+++ tests/__sf_util_truncate_dir.test.fish
@@ -10,19 +10,19 @@ function teardown
 	rm -rf $path
 end
 
-test "truncate path to 1 folder"
+test "Truncate path to 1 directory"
 	'temp3' = (
 		__sf_util_truncate_dir (pwd) 1
 	)
 end
 
-test "truncate path to 3 folders"
+test "Truncate path to 3 directories"
 	'temp1/temp2/temp3' = (
 		__sf_util_truncate_dir (pwd) 3
 	)
 end
 
-test "don't truncate path"
+test "Don't truncate path"
     (pwd) = (
 		__sf_util_truncate_dir (pwd) 0
 	)
