79aaf413f10dadff7c8eb86895fd15e339522fe3
Snuggle <snuggle@sprinkly.net>
2018-07-25 10:16:45 +0100

    Fixed formatting and used spacefish utils
    This should look a lot closer to https://github.com/matchai/spacefish/blob/master/functions/__sf_section_user.fish

--- functions/__sf_section_host.fish
+++ functions/__sf_section_host.fish
@@ -2,26 +2,27 @@
 # Hostname
 #
 
-# ------------------------------------------------------------------------------
-# Configuration
-# ------------------------------------------------------------------------------
-
-set SPACEFISH_HOST_SHOW true
-set SPACEFISH_HOST_SHOW_FULL false
-set SPACEFISH_HOST_PREFIX "at "
-set SPACEFISH_HOST_SUFFIX $SPACEFISH_PROMPT_DEFAULT_SUFFIX
-set SPACEFISH_HOST_COLOR "blue"
-set SPACEFISH_HOST_COLOR_SSH "green"
-
-# ------------------------------------------------------------------------------
-# Section
-# ------------------------------------------------------------------------------
 
 # If there is an ssh connections, current machine name.
-function spacefish_host
-  if test "$SPACEFISH_HOST_SHOW" = 1;
-    return
-  end
+function __sf_section_host -d "Display the current hostname if connected over SSH"
+
+  # ------------------------------------------------------------------------------
+  # Configuration
+  # ------------------------------------------------------------------------------
+
+  __sf_util_set_default SPACEFISH_HOST_SHOW true
+  __sf_util_set_default SPACEFISH_HOST_SHOW_FULL false
+  __sf_util_set_default SPACEFISH_HOST_PREFIX "at "
+  __sf_util_set_default SPACEFISH_HOST_SUFFIX $SPACEFISH_PROMPT_DEFAULT_SUFFIX
+  __sf_util_set_default SPACEFISH_HOST_COLOR "blue"
+  __sf_util_set_default SPACEFISH_HOST_COLOR_SSH "green"
+
+  # ------------------------------------------------------------------------------
+  # Section
+  # ------------------------------------------------------------------------------
+
+  [ "$SPACEFISH_HOST_SHOW" = false ]; and return
+
   if test $SPACEFISH_HOST_SHOW = 'always'; or set -q SSH_CONNECTION;
     set host_color host
 
@@ -34,11 +35,11 @@ function spacefish_host
 
     set host (hostname)
 
-    spacefish::section \
-      "(host_color)" \
-      "(SPACEFISH_HOST_PREFIX)" \
-      "(host)" \
-      "(SPACEFISH_HOST_SUFFIX)"
+    __sf_lib_section \
+      $host_color \
+      $SPACEFISH_HOST_PREFIX \
+      $host \
+      $SPACEFISH_HOST_SUFFIX
       
     end
 end
