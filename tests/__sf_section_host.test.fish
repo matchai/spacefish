0f4ada4d417d569bb228febcf745d26d7334870f
snuggle@sprinkly.net <snuggle@sprinkly.net>
2018-07-25 11:43:48 +0100

    Gah! I broke the tests even more. Will this work?

--- tests/__sf_section_host.test.fish
+++ tests/__sf_section_host.test.fish
@@ -7,12 +7,10 @@ end
 
 test "Correctly shows hostname upon SSH connection"
 	(
-		echo "yes" | ssh localhost
+		echo "yes" | ssh localhost 'echo "Insert Tests Here" && exit'
 		# How to add a check here?
 
-		# Check hostname is correct
 
-		exit
-		# Check hostname doesn't work outside of terminal, test variables
+		# Check hostname doesn't show outside of terminal, test variables
 	) = (__sf_section_host)
 end
