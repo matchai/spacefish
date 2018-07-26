7c5bada5401643bdee2d3219e361c29a9ee64a61
Matan Kushner <hello@matchai.me>
2018-07-25 14:04:17 -1000

    Fix indentation

--- tests/mock.fish
+++ tests/mock.fish
@@ -4,33 +4,33 @@
 #
 
 function mock -a _mock -a exit_code -a code -d "Mock library for fish shell testing"
-  set -l blacklisted "builtin" "functions" "eval" "command"
+	set -l blacklisted "builtin" "functions" "eval" "command"
 
-  if contains $_mock $blacklisted
-    echo The function '"'$_mock'"' is reserved and therefore cannot be mocked.
-    return 1
-  else if not contains $_mock $_mocks
-    function $_mock -V _mock -V exit_code -V code
-      set -l args $argv
+	if contains $_mock $blacklisted
+		echo The function '"'$_mock'"' is reserved and therefore cannot be mocked.
+		return 1
+	else if not contains $_mock $_mocks
+		function $_mock -V _mock -V exit_code -V code
+			set -l args $argv
 
-      # add mocked function to list
-      set -g mocked $_mock $mocked
+			# add mocked function to list
+			set -g mocked $_mock $mocked
 
-      # add exit_code variable
-      if test -z "$exit_code"
-        set exit_code 0
-      end
+			# add exit_code variable
+			if test -z "$exit_code"
+				set exit_code 0
+			end
 
-      if test -z "$code"
-        set code ""
-      end
+			if test -z "$code"
+				set code ""
+			end
 
-      eval $code
-      return $exit_code
-    end
-  end
+			eval $code
+			return $exit_code
+		end
+	end
 end
 
 function unmock -a _mock
-  functions -e $_mock
+	functions -e $_mock
 end
