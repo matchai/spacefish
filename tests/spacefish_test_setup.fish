e434118ebd6727a2db3301c093c527409969c757
Matan Kushner <hello@matchai.me>
2018-07-25 16:36:58 -1000

    Add section tests for node (#51)

--- tests/spacefish_test_setup.fish
+++ tests/spacefish_test_setup.fish
@@ -1,7 +1,12 @@
 function spacefish_test_setup
 	# Delete all lingering spacefish variables
-	set --name | grep SPACEFISH_ | while read -l var
+	set --name | grep -E '^(SPACEFISH_|sf_)' | while read -l var
 		set -e $var
+	end
+
+	# Delete lingering mocked functions
+	for mock in $mocked
+		functions -e $mock
 	end
 
 	# Initialize spacefish theme
