2f9f442756b9c19a0f7b91988bd65bc9559a0aa1
Matan Kushner <hello@matchai.me>
2018-07-28 15:04:19 -1000

    Use POSIX compliant sed for updating version

--- scripts/version.fish
+++ scripts/version.fish
@@ -4,6 +4,7 @@
 set -l new_version (grep -E '"version": "v?([0-9]+\.){1,}' package.json | cut -d\" -f4 2> /dev/null)
 set -l filename $PWD/fish_prompt.fish
 
-sed -i -e "s/set -g SPACEFISH_VERSION .*/set -g SPACEFISH_VERSION $new_version/g" $filename
+sed -e "s/set -g SPACEFISH_VERSION .*/set -g SPACEFISH_VERSION $new_version/g" $filename > $filename.bak
+mv -- $filename.bak $filename
 
 git add fish_prompt.fish
