f5b913a2e7a10547eaab3f72ee505abf52c6f2ce
Matan Kushner <hello@matchai.me>
2018-09-07 21:34:38 -0400

    Increment version

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,7 +1,7 @@
 function fish_prompt
 	# Store the exit code of the last command
 	set -g sf_exit_code $status
-	set -g SPACEFISH_VERSION 0.3.0
+	set -g SPACEFISH_VERSION 1.0.0
 
 	# ------------------------------------------------------------------------------
 	# Configuration
