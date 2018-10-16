f02a646d02183f68ee445af584f55b0236ca0b69
Matan Kushner <hello@matchai.me>
2018-10-16 15:59:51 -0700

    Add omf test run

--- tests/run-fisher.fish
+++ tests/run-fisher.fish
@@ -3,13 +3,13 @@
 set -l gitRoot (git rev-parse --show-toplevel)
 set -l testDir (dirname (status --current-filename))
 set -l tmpDir /tmp/spacefish
+set -gx HOME $tmpDir
 
 # Install fisher if not installed in temporary fish env
-if test ! -f $tmpDir/.config/fish/functions/fisher.fish
+if not test -e $tmpDir/.config/fish/functions/fisher.fish
 	curl https://git.io/fisher --create-dirs -sLo $tmpDir/.config/fish/functions/fisher.fish
 end
 
 # Install fishtape and local spacefish into temp env
-env HOME=$tmpDir fish -c "fisher add fisherman/fishtape $gitRoot"
-env HOME=$tmpDir fish -c "fish_prompt"
-env HOME=$tmpDir fish -c "fishtape $testDir/*.test.fish"
+fish -c "fisher add fisherman/fishtape $gitRoot"
+fish -c "fishtape $testDir/*.test.fish"
---
+++ tests/run-omf.fish
@@ -0,0 +1,19 @@
+#!/usr/bin/env fish
+
+set -l gitRoot (git rev-parse --show-toplevel)
+set -l testDir (dirname (status --current-filename))
+set -l tmpDir /tmp/spacefish
+set -gx HOME $tmpDir
+
+# Install fisher if not installed in temporary fish env
+if not test -d $tmpDir/.config/omf
+	curl -L https://get.oh-my.fish > $tmpDir/install
+	chmod +777 $tmpDir/install
+	fish -c "$tmpDir/install --noninteractive"
+end
+
+# Install fishtape and local spacefish into temp env
+fish -c "rsync -ru $gitRoot \$OMF_PATH/themes --exclude node_modules"
+fish -c "omf theme spacefish"
+fish -c "omf install https://github.com/fisherman/fishtape"
+fish -c "source \$OMF_PATH/pkg/fishtape/*.fish; and fishtape $testDir/*.test.fish"
