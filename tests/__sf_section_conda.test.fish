d63a76d98d613cb423898a02d62b05981095dd1a
jaskaran.bindra <jaskaran.binra@sophos.com>
2018-10-18 19:05:08 +0530

    Fix linting test file

--- tests/__sf_section_conda.test.fish
+++ tests/__sf_section_conda.test.fish
@@ -3,78 +3,78 @@ source $DIRNAME/mock.fish
 set -l LOCAL_CONDA_VERSION 4.5.11
 
 function setup
-  spacefish_test_setup
-  mock conda 0 "echo \"4.5.11\""
-  mkdir -p /tmp/tmp-spacefish
-  cd /tmp/tmp-spacefish
+    spacefish_test_setup
+    mock conda 0 "echo \"4.5.11\""
+    mkdir -p /tmp/tmp-spacefish
+    cd /tmp/tmp-spacefish
 end
 
 function teardown
-  rm -rf /tmp/tmp-spacefish
-  if test "$CONDA_DEFAULT_ENV"
-      set -e CONDA_DEFAULT_ENV
-  end
+    rm -rf /tmp/tmp-spacefish
+    if test "$CONDA_DEFAULT_ENV"
+        set -e CONDA_DEFAULT_ENV
+    end
 end
 
 test "Prints section when conda is installed and CONDA_DEFAULT_ENV is set"
-(
-	set -g CONDA_DEFAULT_ENV some-env
+	(
+		set -g CONDA_DEFAULT_ENV some-env
 
-	set_color --bold fff
-	echo -n "via "
-	set_color normal
-	set_color --bold blue
-	echo -n "🅒 v$LOCAL_CONDA_VERSION"
-	set_color normal
-	set_color --bold fff
-	echo -n " "
-	set_color normal
-) = (__sf_section_conda)
+		set_color --bold fff
+		echo -n "via "
+		set_color normal
+		set_color --bold blue
+		echo -n "🅒 v$LOCAL_CONDA_VERSION"
+		set_color normal
+		set_color --bold fff
+		echo -n " "
+		set_color normal
+	) = (__sf_section_conda)
 end
 
 test "Changing SPACEFISH_CONDA_SYMBOL changes the displayed character"
-(
-	set SPACEFISH_CONDA_SYMBOL "· "
-	set -g CONDA_DEFAULT_ENV some-env
+	(
+		set SPACEFISH_CONDA_SYMBOL "· "
+		set -g CONDA_DEFAULT_ENV some-env
 
-	set_color --bold fff
-	echo -n "via "
-	set_color normal
-	set_color --bold blue
-	echo -n "· v$LOCAL_CONDA_VERSION"
-	set_color normal
-	set_color --bold fff
-	echo -n " "
-	set_color normal
-) = (__sf_section_conda)
+		set_color --bold fff
+		echo -n "via "
+		set_color normal
+		set_color --bold blue
+		echo -n "· v$LOCAL_CONDA_VERSION"
+		set_color normal
+		set_color --bold fff
+		echo -n " "
+		set_color normal
+	) = (__sf_section_conda)
 end
 
 test "Changing SPACEFISH_CONDA_PREFIX changes the character prefix"
-(
-	set SPACEFISH_CONDA_PREFIX ·
-	set -g CONDA_DEFAULT_ENV some-env
+	(
+		set SPACEFISH_CONDA_PREFIX ·
+		set -g CONDA_DEFAULT_ENV some-env
 
-	set_color --bold fff
-	echo -n "·"
-	set_color normal
-	set_color --bold blue
-	echo -n "🅒 v$LOCAL_CONDA_VERSION"
-	set_color normal
-	set_color --bold fff
-	echo -n " "
-	set_color normal
-) = (__sf_section_conda)
+		set_color --bold fff
+		echo -n "·"
+		set_color normal
+		set_color --bold blue
+		echo -n "🅒 v$LOCAL_CONDA_VERSION"
+		set_color normal
+		set_color --bold fff
+		echo -n " "
+		set_color normal
+	) = (__sf_section_conda)
 end
 
 
 # Negative
 test "Doesn't display section when SPACEFISH_CONDA_SHOW is set to 'false'"
-(
-	set -g SPACEFISH_CONDA_SHOW false
-	set -g CONDA_DEFAULT_ENV some-env
-) = (__sf_section_conda)
+	(
+		set -g SPACEFISH_CONDA_SHOW false
+		set -g CONDA_DEFAULT_ENV some-env
+	) = (__sf_section_conda)
 end
 
 test "Doesn't display section when CONDA_DEFAULT_ENV is not set"
-() = (__sf_section_conda)
+	() = (__sf_section_conda)
 end
