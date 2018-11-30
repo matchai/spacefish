709f686f256062288d3a5ed786d68a761bc7824d
Matan Kushner <hello@matchai.me>
2018-11-29 20:58:00 -0500

    Make branch util tests consistent

--- tests/__sf_util_git_branch.test.fish
+++ tests/__sf_util_git_branch.test.fish
@@ -1,46 +1,45 @@
-set path /tmp/$DIRNAME/$TESTNAME
+source $DIRNAME/spacefish_test_setup.fish
 
 function setup
-	mkdir -p $path/tempGit
-	cd $path/tempGit
-end
-
-function teardown
-	rm -rf $path
-end
-
-function git_init
-	command git init
+	spacefish_test_setup
+	mkdir -p /tmp/tmp-spacefish
+	cd /tmp/tmp-spacefish
+	command git init >/dev/null
 	command git config --local user.email "test@example.com"
 	command git config --local user.name "Test User"
 end
 
+function teardown
+	rm -rf /tmp/tmp-spacefish
+end
+
 test "Identifies HEAD before initial commit"
-	'HEAD' = (
-		command git init
-		__sf_util_git_branch
-	)
+	(
+		echo "HEAD"
+	) = (__sf_util_git_branch)
 end
 
 test "Identifies master branch"
-	'master' = (
-		git_init
-		command git commit --allow-empty -m "initial commit"
-		__sf_util_git_branch
-	)
+	(
+		command git commit --allow-empty -m "initial commit" --quiet
+
+		echo "master"
+	) = (__sf_util_git_branch)
 end
 
 test "Identifies an alternate branch name"
-	'testBranch' = (
-		git_init
+	(
 		command git checkout -b "testBranch" ^/dev/null
-		command git commit --allow-empty -m "initial commit"
-		__sf_util_git_branch
-	)
+		command git commit --allow-empty -m "initial commit" --quiet
+
+		echo "testBranch"
+	) = (__sf_util_git_branch)
 end
 
 test "No result provided for non-git directory"
-    '' = (
-		__sf_util_git_branch
-	)
+	(
+		cd ~
+
+		echo ""
+	) = (__sf_util_git_branch)
 end
