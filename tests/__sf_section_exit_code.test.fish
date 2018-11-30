091478d9812511452abfbc246994074714a19e31
Matan Kushner <hello@matchai.me>
2018-11-29 20:59:01 -0500

    Fix some whitespace

--- tests/__sf_section_exit_code.test.fish
+++ tests/__sf_section_exit_code.test.fish
@@ -6,16 +6,16 @@ end
 
 test "Exit code not enabled by default"
 	(
-	set sf_exit_code 1
+		set sf_exit_code 1
 	) = (__sf_section_exit_code)
 end
 
 test "Enable exit-code, shows exit code upon fail"
 	(
-	set SPACEFISH_EXIT_CODE_SHOW true
-	set sf_exit_code 1
+		set SPACEFISH_EXIT_CODE_SHOW true
+		set sf_exit_code 1
 
-	set_color --bold fff
+		set_color --bold fff
 		set_color normal
 		set_color --bold red
 		echo -n "✘1"
@@ -28,18 +28,18 @@ end
 
 test "Hides exit code upon success"
 	(
-	set SPACEFISH_EXIT_CODE_SHOW true
+		set SPACEFISH_EXIT_CODE_SHOW true
 		set sf_exit_code 0
 	) = (__sf_section_exit_code)
 end
 
 test "Color-changing exit code"
 	(
-	set SPACEFISH_EXIT_CODE_SHOW true
-	set SPACEFISH_EXIT_CODE_COLOR "purple"
-	set sf_exit_code 1
+		set SPACEFISH_EXIT_CODE_SHOW true
+		set SPACEFISH_EXIT_CODE_COLOR "purple"
+		set sf_exit_code 1
 
-	set_color --bold fff
+		set_color --bold fff
 		set_color normal
 		set_color --bold purple
 		echo -n "✘1"
