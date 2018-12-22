5a48dc9256d21812b0b7d6fd24c78ed2bb536d49
Samantha Enders <ladysamantha@protonmail.com>
2018-12-22 11:28:13 -0700

    tests(dotnet): add tests for dotnet files

--- tests/__sf_section_dotnet.test.fish
+++ tests/__sf_section_dotnet.test.fish
@@ -5,7 +5,6 @@ function setup
 	spacefish_test_setup
 	mock dotnet 0 "echo \"2.1.403\""
 	mkdir -p /tmp/tmp-spacefish
-	touch /tmp/tmp-spacefish/project.json
 	cd /tmp/tmp-spacefish
 end
 
@@ -13,9 +12,79 @@ function teardown
 	rm -rf /tmp/tmp-spacefish
 end
 
-test "Prints section"
+test "Prints nothing when required files are missing"
 	(
-		touch
+		rm -f /tmp/tmp-spacefish/project.json
+		rm -f /tmp/tmp-spacefish/global.json
+		rm -f '/tmp/tmp-spacefish/*.sln'
+		rm -f '/tmp/tmp-spacefish/*.csproj'
+		rm -f '/tmp/tmp-spacefish/*.xproj'
+	) = (__sf_section_dotnet)
+end
+
+test "Prints section if project.json is present"
+	(
+		touch /tmp/tmp-spacefish/project.json
+		set_color --bold fff
+		echo -n "via "
+		set_color normal
+		set_color --bold af00d7
+		echo -n ".NET 2.1.403"
+		set_color normal
+		set_color --bold fff
+		echo -n " "
+		set_color normal
+	) = (__sf_section_dotnet)
+end
+
+test "Prints section if global.json is present"
+	(
+		touch /tmp/tmp-spacefish/global.json
+		set_color --bold fff
+		echo -n "via "
+		set_color normal
+		set_color --bold af00d7
+		echo -n ".NET 2.1.403"
+		set_color normal
+		set_color --bold fff
+		echo -n " "
+		set_color normal
+	) = (__sf_section_dotnet)
+end
+
+test "Prints section if a .csproj file is present"
+	(
+		touch /tmp/tmp-spacefish/tmp.csproj
+		set_color --bold fff
+		echo -n "via "
+		set_color normal
+		set_color --bold af00d7
+		echo -n ".NET 2.1.403"
+		set_color normal
+		set_color --bold fff
+		echo -n " "
+		set_color normal
+	) = (__sf_section_dotnet)
+end
+
+test "Prints section if a .xproj file is present"
+	(
+		touch /tmp/tmp-spacefish/tmp.xproj
+		set_color --bold fff
+		echo -n "via "
+		set_color normal
+		set_color --bold af00d7
+		echo -n ".NET 2.1.403"
+		set_color normal
+		set_color --bold fff
+		echo -n " "
+		set_color normal
+	) = (__sf_section_dotnet)
+end
+
+test "Prints section if a .sln file is present"
+	(
+		touch /tmp/tmp-spacefish/tmp.sln
 		set_color --bold fff
 		echo -n "via "
 		set_color normal
@@ -30,6 +99,7 @@ end
 
 test "Changing SPACEFISH_DOTNET_SYMBOL changes the displayed character"
 	(
+		touch /tmp/tmp-spacefish/tmp.sln
 		set SPACEFISH_DOTNET_SYMBOL "· "
 
 		set_color --bold fff
@@ -46,6 +116,7 @@ end
 
 test "Changing SPACEFISH_DOTNET_PREFIX changes the character prefix"
 	(
+		touch /tmp/tmp-spacefish/tmp.sln
 		set sf_exit_code 0
 		set SPACEFISH_DOTNET_PREFIX ·
 
@@ -63,6 +134,7 @@ end
 
 test "Changing SPACEFISH_DOTNET_SUFFIX changes the character prefix"
 	(
+		touch /tmp/tmp-spacefish/tmp.sln
 		set sf_exit_code 0
 		set SPACEFISH_DOTNET_SUFFIX ·
 
