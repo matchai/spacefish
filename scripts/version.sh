8eb686dcfecc0ae45e2afb286be76cab2b494731
Matan Kushner <hello@matchai.me>
2018-12-27 21:44:47 -0500

    fix: remove git add from version script
    It appeared that @semantic-release/git was not committing files that were already being staged, so this will now trigger version incrementation

--- scripts/version.sh
+++ scripts/version.sh
@@ -6,4 +6,3 @@ filename=$(pwd)/fish_prompt.fish
 sed -e "s/set -g SPACEFISH_VERSION .*/set -g SPACEFISH_VERSION $new_version/g" $filename > $filename.bak
 mv -- $filename.bak $filename
 
-git add fish_prompt.fish
