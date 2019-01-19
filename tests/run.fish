3610fce74deb685b63c4363e300fb39bda4cbb75
Matan Kushner <hello@matchai.me>
2019-01-19 15:23:50 -0500

    fish_indent test setup files

--- tests/run.fish
+++ tests/run.fish
@@ -6,7 +6,7 @@ set -l tmpDir /tmp/spacefish
 
 # Install fisher if not installed in temporary fish env
 if test ! -f $tmpDir/.config/fish/functions/fisher.fish
-	curl https://git.io/fisher --create-dirs -sLo $tmpDir/.config/fish/functions/fisher.fish
+    curl https://git.io/fisher --create-dirs -sLo $tmpDir/.config/fish/functions/fisher.fish
 end
 
 # Install fishtape and local spacefish into temp env
@@ -14,9 +14,9 @@ env HOME=$tmpDir fish -c "fisher add jorgebucaran/fishtape matchai/fish-mock $gi
 env HOME=$tmpDir fish -c "fish_prompt"
 
 if test (count $argv) -gt 0
-	# Run an individual test file if it is provided as an argument
-	env HOME=$tmpDir fish -c "fishtape $argv[1]"
+    # Run an individual test file if it is provided as an argument
+    env HOME=$tmpDir fish -c "fishtape $argv[1]"
 else
-	# Otherwise run all test files
-	env HOME=$tmpDir fish -c "fishtape $testDir/*.test.fish"
+    # Otherwise run all test files
+    env HOME=$tmpDir fish -c "fishtape $testDir/*.test.fish"
 end
--- tests/spacefish_test_setup.fish
+++ tests/spacefish_test_setup.fish
@@ -1,14 +1,14 @@
 function spacefish_test_setup
-	# Delete all lingering spacefish variables
-	set --name | grep -E '^(SPACEFISH_|sf_)' | while read -l var
-		set -e $var
-	end
+    # Delete all lingering spacefish variables
+    set --name | grep -E '^(SPACEFISH_|sf_)' | while read -l var
+        set -e $var
+    end
 
-	# Delete lingering mocked functions
-	for mock in $_mocked
-		unmock $mock
-	end
+    # Delete lingering mocked functions
+    for mock in $_mocked
+        unmock $mock
+    end
 
-	# Initialize spacefish theme
-	fish_prompt>/dev/null
+    # Initialize spacefish theme
+    fish_prompt >/dev/null
 end
