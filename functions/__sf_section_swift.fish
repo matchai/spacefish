80868ea0115275e2211b270673d31a32d4a20d7f
Ryan Collins <ryan@ryanmcollins.com>
2019-01-29 14:52:30 -0500

    Add Swift Section

---
+++ functions/__sf_section_swift.fish
@@ -0,0 +1,39 @@
+#
+# Swift
+#
+# Swift Swift makes it easy to write software that is incredibly fast and safe by design.
+# Link: https://swift.org
+
+function __sf_section_swift -d "Display the current swift version if you are inside a path that contains a Swift package"
+	# ------------------------------------------------------------------------------
+	# Configuration
+	# ------------------------------------------------------------------------------
+
+	__sf_util_set_default SPACEFISH_SWIFT_SHOW true
+	__sf_util_set_default SPACEFISH_SWIFT_PREFIX $SPACEFISH_PROMPT_DEFAULT_PREFIX
+	__sf_util_set_default SPACEFISH_SWIFT_SUFFIX $SPACEFISH_PROMPT_DEFAULT_SUFFIX
+	__sf_util_set_default SPACEFISH_SWIFT_SYMBOL "🦜 "
+	__sf_util_set_default SPACEFISH_SWIFT_COLOR cyan
+
+	# ------------------------------------------------------------------------------
+	# Section
+	# ------------------------------------------------------------------------------
+
+	# Show the current version?
+	[ $SPACEFISH_SWIFT_SHOW = false ]; and return
+
+	# Ensure the swift command is available
+	type -q swift; or return
+
+	if not test -f Package.swift \
+		return
+	end
+
+	set -l swift_version (string sub -s 1 (swift --version | string split ' ')[4])
+
+	__sf_lib_section \
+		$SPACEFISH_SWIFT_COLOR \
+		$SPACEFISH_SWIFT_PREFIX \
+		"$SPACEFISH_SWIFT_SYMBOL""$go_version" \
+		$SPACEFISH_SWIFT_SUFFIX
+end
