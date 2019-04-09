242b25b9b474da5d2a26447559441865e13ac675
Nam Nguyen <nam.nguyen.de@gmail.com>
2019-04-09 14:44:02 +0200

    Update __sf_section_kubecontext.fish

--- functions/__sf_section_kubecontext.fish
+++ functions/__sf_section_kubecontext.fish
@@ -11,12 +11,14 @@ function __sf_section_kubecontext -d "Display the kubernetes context"
 	# ------------------------------------------------------------------------------
 
 	__sf_util_set_default SPACEFISH_KUBECONTEXT_SHOW true
+	__sf_util_set_default SPACESHIP_KUBECONTEXT_NAMESPACE_SHOW true
 	__sf_util_set_default SPACEFISH_KUBECONTEXT_PREFIX "at "
 	__sf_util_set_default SPACEFISH_KUBECONTEXT_SUFFIX $SPACEFISH_PROMPT_DEFAULT_SUFFIX
 	# Additional space is added because ☸️ is wider than other symbols
 	# See: https://github.com/denysdovhan/spaceship-prompt/pull/432
 	__sf_util_set_default SPACEFISH_KUBECONTEXT_SYMBOL "☸️  "
 	__sf_util_set_default SPACEFISH_KUBECONTEXT_COLOR cyan
+
 
 	# ------------------------------------------------------------------------------
 	# Section
@@ -31,14 +33,16 @@ function __sf_section_kubecontext -d "Display the kubernetes context"
 	set -l kube_context (kubectl config current-context 2>/dev/null)
 	[ -z $kube_context ]; and return
 	
-	set -l kube_context_ns (kubectl config view -o jsonpath='{.contexts[].context.namespace}' 2>/dev/null)
-	[ -z $kube_context_ns ]; and return
-
+	if test "$SPACESHIP_KUBECONTEXT_NAMESPACE_SHOW" = "true"
+		set kube_namespace (kubectl config view --minify --output 'jsonpath={..namespace}' 2>/dev/null)
+		if test $kube_context != "default"
+			set kube_context "$kube_context ($kube_namespace)"
+		end
+	end
 
 	__sf_lib_section \
 		$SPACEFISH_KUBECONTEXT_COLOR \
 		$SPACEFISH_KUBECONTEXT_PREFIX \
 		"$SPACEFISH_KUBECONTEXT_SYMBOL""$kube_context" \
-	  " as "$kube_context_ns\
 		$SPACEFISH_KUBECONTEXT_SUFFIX
 end
